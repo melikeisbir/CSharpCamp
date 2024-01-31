1ade1c292e91b2c8cbb5b35fdcbb3a6c45729d55
Melike İsbir <melikeisbr@gmail.com>
2024-01-31 16:41:44 +0300

    Add project files.

---
+++ CSharpCamp/Program.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCamp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
+ 
