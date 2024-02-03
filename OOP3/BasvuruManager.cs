f11daf13585e2e0c303ab5542b8cfd1aeab3ef2e
Melike İsbir <melikeisbr@gmail.com>
2024-02-03 15:09:35 +0300

    Interface

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    public class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager)
+        {
+            //KonutKrediManager konutKrediManager = new KonutKrediManager(); // Tüm basvurular konutkredisine bagımlı oldu
+            //konutKrediManager.Hesapla();
+            //Console.WriteLine();
+
+            krediManager.Hesapla();
+        }
+        public void KrediOnBilgilendirme(List<IKrediManager> krediler) //birden fazla kredinin hesabı için
+        {
+            foreach (var item in krediler)
+            {
+                item.Hesapla(); //listedeki her bir kredinin hesabını yap
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+            basvuruManager.KrediOnBilgilendirme(krediler);
+        }
+    }
+}
