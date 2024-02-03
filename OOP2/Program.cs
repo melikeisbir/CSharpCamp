508153ffe9a298702e09a6202b44b927c69b0f7b
Melike İsbir <melikeisbr@gmail.com>
2024-02-03 13:31:31 +0300

    OOP2 Coorporate, Individual, Business Classes, Inheritance

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Meli";
+            musteri1.Soyadi = "İs";
+            musteri1.TcNo = "12345678900";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "23234";
+            musteri2.SirketAdi = "Kod";
+            musteri2.VergiNo = "232345564";
+
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+        }
+    }
+}
