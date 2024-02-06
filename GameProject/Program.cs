e9bb4b9215a7ca7b220edbd816ad5021886efbe2
Melike İsbir <melikeisbr@gmail.com>
2024-02-06 15:41:59 +0300

    GameProject

---
+++ GameProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 2001,
+                FirstName = "Melike",
+                LastName = "İsb",
+                IdentityNumber = 12345
+            });
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 2001 && gamer.FirstName == "Melike" && gamer.LastName == "İsb" && gamer.IdentityNumber == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
